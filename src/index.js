4cdf7d69945fa5e3aed071a3ff756ced7da9098e
roms13 <romain.kelifa@gmail.com>
2019-08-29 14:11:24 +0700

    :arrow_up: Update dependencies

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ const generate = async () => {
   try {
     const { gitmojis } = await fetch()
     await commitlint(gitmojis)
-    console.log(`succesfully auto-generated commitlint rules from https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json`)
+    console.log('succesfully auto-generated commitlint rules from https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json')
   } catch (e) {
-    console.log(`error trying to auto-generate commitlint rules from https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json`)
+    console.log('error trying to auto-generate commitlint rules from https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json')
   }
 }
 generate()
